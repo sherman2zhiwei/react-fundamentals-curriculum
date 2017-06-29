09e91a19c066e85e146febb16b23180f13969659
Wong Zhiwei <sherman2zhiwei@yahoo.com>
2017-06-29 17:33:52 +0800

    Added search form to navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,9 +1,11 @@
 var React = require('react');
+var SearchLocationForm = require('./SearchLocationForm');
 
 module.exports = function Navbar(props){
 	return(
 		<div className="navbar">
 			<h1>Clever Title</h1>
+			<SearchLocationForm orientation="row"/>
 		</div>
 	)
 };
